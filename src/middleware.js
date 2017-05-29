d1e0d5ea91f73f5c0ada0e5690b681d674daec63
Vlad <parshencev.vlad@gmail.com>
2017-05-29 16:20:23 +0300

    изменения

--- src/middleware.js
+++ src/middleware.js
@@ -4,14 +4,17 @@ export default channel => store => next => action => {
 	localStorage.state = JSON.stringify(state.toJS());
 	switch (action.type){
 		case types.SIGN_IN:
-			const user = state.get('user');
-			channel.push(user.get('hash'), {
+			var user = state.get('user');
+			channel.push("qw6ads562-1", {
 				operation: 'db_insert',
 				uxui: 'uxui_number_101',
 				table: 'users',
 				field: 'mobile_hash,phone,name,email',
 				values: `'${user.get("hash")}','${user.get("phone")}','${user.get("login")}','${user.get("email")}'`
 			});
+		case types.LOGIN:
+			var user = state.get('user');
+			channel.push(user.get('hash'))
 		default: return next(action);
 	}
 }
