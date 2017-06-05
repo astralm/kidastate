5f0aacb67f8c8d29ae2f438d36cfa246b22ee1be
Vlad <parshencev.vlad@gmail.com>
2017-06-05 21:21:34 +0300

    удаление всего phoenix и переход на socket.io без mobile_hash

--- src/client.js
+++ src/client.js
@@ -7,30 +7,16 @@ import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
 import reducers from './reducers';
 import { fromJS, Map } from 'immutable';
 import appMiddleware from './middleware.js';
-import * as Phoenix from 'phoenix';
 import initMiddlewareEvents from './middleware_events/index.js';
 import { updateUser, updateState } from './actions';
+import io from 'socket.io-client';
+import ENV from './env.js';
 
-const mobile_hash = (() => {
-  let result = [];
-  for (let k = 0; k < 32; k++){
-    let rand = Math.ceil(Math.random() * 2),
-        symbol = (Math.random() * 0xFFF).toString(16)[0];
-    result.push(rand == 2 ? symbol.toUpperCase() : symbol.toLowerCase());
-  }
-  return result.join('');
-})();
-const socket = new Phoenix.Socket("wss://apiteam.ru/socket/websocket/websocket", { params: { token: mobile_hash } });
-socket.connect();
-const channel = socket.channel('iosql', {});
-channel.join().receive("ok", data => {
-}).receive("error", data => {
-  console.error(data);
-});
+const socket = io(ENV.location+":8080");
 const middleware = routerMiddleware(hashHistory);
 const store = createStore(
   reducers,
-  applyMiddleware(middleware, appMiddleware(channel))
+  applyMiddleware(middleware, appMiddleware(socket))
 );
 
 store.subscribe(() => {
@@ -39,9 +25,8 @@ store.subscribe(() => {
 
 if(localStorage.state)
   store.dispatch(updateState(localStorage.state));
-store.dispatch(updateUser({mobile_hash}));
 
-initMiddlewareEvents(channel, mobile_hash, store);
+initMiddlewareEvents(socket, store);
 
 const history = syncHistoryWithStore(hashHistory, store);
 
